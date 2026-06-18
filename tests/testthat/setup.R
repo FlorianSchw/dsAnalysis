8dba6964585abb5f0c89334b51b700b6bedac7c5
FlorianSchw <Florian.Schwarz@dife.de>
2026-06-18 02:05:45 +0200

    test: work-in-progress on test-initMockData

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,4 +2,4 @@
 #### importing config file for different test setup
 cfg_dir_overwrite <- FALSE
 cfg_dir_overwrite <- config::get(value = "test-initialize_project-overwriting",
-                                 file = here::here("tests/testthat", "config.yml"))
+                                 file = testthat::test_path("config-testing.yml"))
